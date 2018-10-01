063ee9a4774b52af52c706000b12bb1a1be2cc31
Milan Grahovac <grahovac85@gmail.com>
2018-10-01 16:10:11 +0200

    head1

---
+++ head-01/returnTwoStrings/return2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Names() (first string, second string) {
+	first = "Foo"
+	second = "Bar"
+	return
+}
+
+func main() {
+
+	n1, n2 := Names()
+	fmt.Printf("n1=%s, n2=%s\n", n1, n2)
+
+	n3, _ := Names()
+	fmt.Printf("n3=%s\n", n3)
+}
