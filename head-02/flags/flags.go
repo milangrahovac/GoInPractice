a0d5546439a776d7cdd8a051091f90ede278ace6
Milan Grahovac <grahovac85@gmail.com>
2018-10-03 11:48:32 +0200

    flags

---
+++ head-02/flags/flags.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var name = flag.String("name", "World", "A name to say hello to.")
+var russian bool
+var spanish bool
+
+func init() {
+	flag.BoolVar(&russian, "russian", false, "Use russian language.")
+	flag.BoolVar(&russian, "r", false, "Use russian language.")
+	flag.BoolVar(&spanish, "spanish", false, "Use spanish language.")
+	flag.BoolVar(&spanish, "s", false, "Use spanish language.")
+}
+
+func main() {
+	flag.Parse()
+	if russian == true {
+		fmt.Printf("Привет %s!\n", *name)
+	} else if spanish == true {
+		fmt.Printf("Hola %s!\n", *name)
+	} else {
+		fmt.Printf("Hello %s!\n", *name)
+	}
+}
