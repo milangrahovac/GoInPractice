094197c09dce31ffc5474899d2af54a0cc079ac3
Milan Grahovac <grahovac85@gmail.com>
2018-10-04 14:34:32 +0200

    cli

--- head-02/cli-flags/hello-cli.go
+++ head-02/cli-flags/hello-cli.go
@@ -1,3 +1,31 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/codegangsta/cli"
+// )
+
+// func main() {
+// 	app := cli.NewApp()
+// 	app.Name = "hello-cli"
+// 	app.Usage = "Print hello world"
+// 	app.Flags = []cli.Flag{
+// 		cli.StringFlag{
+// 			Name:  "name, n",
+// 			Value: "World",
+// 			Usage: "Who to say hello to",
+// 		},
+// 	}
+// 	app.Action = func(c *cli.Context) error {
+// 		name := c.GlobalString("name")
+// 		fmt.Printf("Hello %s!\n", name)
+// 		return nil
+// 	}
+// 	app.Run(os.Args)
+// }
+
 package main
 
 import (
@@ -17,10 +45,29 @@ func main() {
 			Value: "World",
 			Usage: "Who to say hello to",
 		},
+		cli.BoolFlag{
+			Name:   "russian, r",
+			Hidden: false,
+			Usage:  "Use russian language.",
+		},
+		cli.BoolFlag{
+			Name:   "spanish, s",
+			Hidden: false,
+			Usage:  "Use spanish language.",
+		},
 	}
 	app.Action = func(c *cli.Context) error {
 		name := c.GlobalString("name")
-		fmt.Printf("Hello %s!\n", name)
+		russian := c.GlobalBool("russian")
+		spanish := c.GlobalBool("spanish")
+
+		if russian {
+			fmt.Printf("Привет %s!\n", name)
+		} else if spanish {
+			fmt.Printf("Hola %s!\n", name)
+		} else {
+			fmt.Printf("Hello %s!\n", name)
+		}
 		return nil
 	}
 	app.Run(os.Args)
