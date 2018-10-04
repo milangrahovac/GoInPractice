b84ee2983658d6679fccb4dcc8661a7b5f5f0590
Milan Grahovac <grahovac85@gmail.com>
2018-10-04 14:35:41 +0200

    cli

--- head-02/cli-flags/hello-cli.go
+++ head-02/cli-flags/hello-cli.go
@@ -1,31 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-
-// 	"github.com/codegangsta/cli"
-// )
-
-// func main() {
-// 	app := cli.NewApp()
-// 	app.Name = "hello-cli"
-// 	app.Usage = "Print hello world"
-// 	app.Flags = []cli.Flag{
-// 		cli.StringFlag{
-// 			Name:  "name, n",
-// 			Value: "World",
-// 			Usage: "Who to say hello to",
-// 		},
-// 	}
-// 	app.Action = func(c *cli.Context) error {
-// 		name := c.GlobalString("name")
-// 		fmt.Printf("Hello %s!\n", name)
-// 		return nil
-// 	}
-// 	app.Run(os.Args)
-// }
-
 package main
 
 import (
