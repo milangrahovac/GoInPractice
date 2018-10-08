25a8863ecaa6d4d112c3b800bfdac9bd2c65977d
Milan Grahovac <grahovac85@gmail.com>
2018-10-08 22:20:17 +0200

    fix simple server

--- head-02/simple_server/simple_server.go
+++ head-02/simple_server/simple_server.go
@@ -5,15 +5,16 @@ import (
 	"strings"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
-	w.Write([]byte(message))
-}
 func main() {
 	http.HandleFunc("/", sayHello)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
 }
