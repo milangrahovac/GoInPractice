5842cbd73e677956c3ae937472fc4caae753e324
Milan Grahovac <grahovac85@gmail.com>
2018-10-11 19:14:24 +0200

    fix multiple handlers

--- head-02/multiple_handlers/multiple_handlers.go
+++ head-02/multiple_handlers/multiple_handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -9,5 +10,13 @@ func main() {
 	http.HandleFunc("/goodbuy/", goodbuy)
 	http.HandleFunc("/", homePage)
 	http.ListenAndServe(":8080", nil)
+}
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Inigo Montoya"
+	}
+	fmt.Fprint(w, "Hello, my name is ", name)
 }
