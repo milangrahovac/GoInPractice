e919f85aea656f5e260b969311c1a08aacfab349
Milan Grahovac <grahovac85@gmail.com>
2018-10-15 21:01:28 +0200

    add path_handlers

--- head-02/path_handlers/path_handlers.go
+++ head-02/path_handlers/path_handlers.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+type pathResolver struct {
+	handlers map[string]http.HandlerFunc
+}
 
 func main() {
 	fmt.Println("hello world")
+	pr := newPathResolver()
+}
+
+func newPathResolver() *pathResolver {
+	return &pathResolver{make(map[string]http.HandlerFunc)}
+}
+
+func (p *pathResolver) Add(path string, handler http.HandlerFunc) {
+	p.handlers[path] = handler
 }
