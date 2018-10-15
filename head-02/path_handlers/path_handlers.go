cfa879695694322a1af8ac8007a932fbcc1ac435
Milan Grahovac <grahovac85@gmail.com>
2018-10-15 21:13:35 +0200

    add func servehttp in path_handlers.go

--- head-02/path_handlers/path_handlers.go
+++ head-02/path_handlers/path_handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path"
 )
 
 type pathResolver struct {
@@ -12,6 +13,9 @@ type pathResolver struct {
 func main() {
 	fmt.Println("hello world")
 	pr := newPathResolver()
+	pr.Add("GET /hello", hello)
+	pr.Add("* /goodbuy*", goodbuy)
+
 }
 
 func newPathResolver() *pathResolver {
@@ -20,4 +24,17 @@ func newPathResolver() *pathResolver {
 
 func (p *pathResolver) Add(path string, handler http.HandlerFunc) {
 	p.handlers[path] = handler
+}
+
+func (p *pathResolver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	check := req.Method + " " + req.URL.Path
+	for pattern, handlerFunc := range p.handlers {
+		if ok, err := path.Match(pattern, check); ok && err == nil {
+			handlerFunc(res, req)
+			return
+		} else if err != nil {
+			fmt.Fprintln(res, req)
+		}
+	}
+	http.NotFound(res, req)
 }
