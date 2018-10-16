c9feb482048538b20bcc6b67080ebfe12abd1c66
Milan Grahovac <grahovac85@gmail.com>
2018-10-16 19:33:18 +0200

    fix regex

--- head-02/regex_handlers/regex_handlers.go
+++ head-02/regex_handlers/regex_handlers.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	rr := newPathResolver()
+	rr.Add("GET /hello", hello)
+	rr.Add("(GET|HEAD) /goodbye(/?[A-Za-z0-9]*?)", goodbye)
+	http.ListenAndServe(":8080", rr)
 }
