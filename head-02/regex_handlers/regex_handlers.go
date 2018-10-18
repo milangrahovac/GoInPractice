ce1b620e0eea878f8f3aed0d569fd361adbc9772
Milan Grahovac <grahovac85@gmail.com>
2018-10-18 20:41:46 +0200

    fix regex

--- head-02/regex_handlers/regex_handlers.go
+++ head-02/regex_handlers/regex_handlers.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"net/http"
+	"regexp"
 )
+
+type regexResolver struct {
+	handlers map[string]http.HandlerFunc
+	cache    map[string]*regexp.Regexp
+}
 
 func main() {
 	rr := newPathResolver()
 	rr.Add("GET /hello", hello)
 	rr.Add("(GET|HEAD) /goodbye(/?[A-Za-z0-9]*?)", goodbye)
 	http.ListenAndServe(":8080", rr)
+}
 
+func newPathResolver() *regexResolver {
+	return &regexResolver{
+		handlers: make(map[string]http.HandleFunc),
+		cache:    make(map[string]*regexp.Regexp),
+	}
 }
