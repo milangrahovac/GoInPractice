7ee9d2548661d6e03132158dd78782d3510082bb
Milan Grahovac <grahovac85@gmail.com>
2018-10-26 20:41:19 +0200

    fix goroutine

--- head-03/goroutine/goroutine.go
+++ head-03/goroutine/goroutine.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("Outside a goroutine.")
 	go func() {
-		fmt.Println("Inside a goroutine")
+		fmt.Println("Inside a goroutine.")
 	}()
 	fmt.Println("Outside again.")
 	runtime.Gosched()
